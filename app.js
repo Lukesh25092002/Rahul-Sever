bfce03d83fa10efe3616d38df3ec3b568eadafb2
Lukesh <lukeshpatil05@gmail.com>
2022-09-11 00:26:29 +0530

    hgg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get('/myendpoint', (req, res) => {
+app.get('/home', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
   });
 
